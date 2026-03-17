7d049c10b16c23fd4bd2c259ccac18cac4fc3576
Miro Dojkic <mdojkic@gmail.com>
2026-03-17 16:19:05 +0200

    fix cursor blink

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.guicursor = "n-v-c-sm:block-blinkon500-blinkoff500,"
+  .. "i-ci-ve:ver25,"
+  .. "r-cr-o:hor20,"
+  .. "t:block-blinkon500-blinkoff500-TermCursor"
